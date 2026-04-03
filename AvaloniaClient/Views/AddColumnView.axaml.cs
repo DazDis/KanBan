e500038044ced197df4bac513d0246d5805cd273
AABaxchivanzhi <alya.baxchivanzhi@bk.ru>
2026-04-03 22:31:03 +0300

    add "addColumnView"

---
+++ AvaloniaClient/Views/AddColumnView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaClient;
+
+public partial class AddColumnView : UserControl
+{
+    public AddColumnView()
+    {
+        InitializeComponent();
+    }
+}
