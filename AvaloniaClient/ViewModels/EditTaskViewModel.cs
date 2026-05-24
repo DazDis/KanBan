5cd346c769bcada06db5de8ec2c110a79c1e111c
ChiCha <janus-planus@rambler.ru>
2026-05-25 01:28:36 +0300

    Вернул сохранение тегов

--- AvaloniaClient/ViewModels/EditTaskViewModel.cs
+++ AvaloniaClient/ViewModels/EditTaskViewModel.cs
@@ -160,7 +160,10 @@ namespace AvaloniaClient.ViewModels
             SaveCommand = ReactiveCommand.CreateFromTask(async () =>
             {
                 var changes = new List<(string action, string old, string _new)>();
-
+                _task.UserIds = SelectedUser != null ? new List<int?> { SelectedUser.Id } : new();
+                _task.LabelIds = Labels.Where(x => x.IsSelected).Select(x => (int?)x.Id).ToList();
+                _task.TeamIds = SelectedTeam != null ? new List<int?> { SelectedTeam.Id } : new();
+                _task.Labels = new ObservableCollection<LabelModel>(Labels.Where(x => x.IsSelected));
                 if (Title != _originalTitle)
                     changes.Add(("Название", _originalTitle ?? "", Title ?? ""));
 
@@ -176,14 +179,12 @@ namespace AvaloniaClient.ViewModels
                 if (SelectedColor != _originalColor)
                     changes.Add(("Цвет", _originalColor ?? "По умолчанию", SelectedColor ?? ""));
 
-                // Применяем изменения
                 _task.Title = Title;
                 _task.Description = Description;
                 _task.Deadline = Deadline;
                 _task.ColumnId = SelectedColumnStatus?.Id ?? _columnId;
                 _task.Color = SelectedColor;
 
-                // Записываем историю на сервер
                 foreach (var change in changes)
                 {
                     await _taskService.AddHistoryEntryAsync(_task.Id, change.action, change.old, change._new);
