83dbd9e2a605cae40bb43567cbbb1159bb7db6e4
AABaxchivanzhi <alya.baxchivanzhi@bk.ru>
2026-05-24 19:42:17 +0300

    .

--- AvaloniaClient/Models/TeamModel.cs
+++ AvaloniaClient/Models/TeamModel.cs
@@ -7,14 +7,21 @@ namespace AvaloniaClient.DataBase
     public class TeamModel : ReactiveObject
     {
         private int _id;
-        private string _title;
+        private string _title = "";
+        private string _color = "#CCCCCC";
         private List<int> _userIds = new();
-        private ObservableCollection<UserModel?> _users = new();
+        private ObservableCollection<UserModel> _users = new();
 
         public int Id
         {
             get => _id;
             set => this.RaiseAndSetIfChanged(ref _id, value);
+        }
+
+        public string Color
+        {
+            get => _color;
+            set => this.RaiseAndSetIfChanged(ref _color, value);
         }
 
         public string Title
